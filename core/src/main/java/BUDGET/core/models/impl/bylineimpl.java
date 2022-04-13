5b080c227e2c8fb726d91375c18cf4b5c8103065
Ashish Alok <ashishalok12345@gmail.com>
2022-04-13 10:10:21 +0530

    ashish

---
+++ core/src/main/java/BUDGET/core/models/impl/bylineimpl.java
@@ -0,0 +1,22 @@
+package BUDGET.core.models.impl;
+import java.util.List;
+import BUDGET.core.models.byline;
+public class bylineimpl implements byline{
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> getOccupations() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
