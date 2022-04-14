d959a7d68e6d893343b262e850b038b1f34831c3
jauntyjay09 <jayakumar2001mhk@gmail.com>
2022-04-14 13:05:14 +0530

    sling  changes

---
+++ core/src/main/java/BUDGET/core/models/SampleModel.java
@@ -0,0 +1,76 @@
+
+package BUDGET.core.models;
+
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.settings.SlingSettingsService;
+
+import com.adobe.agl.impl.StringUCharacterIterator;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+import java.util.Optional;
+
+@Model(adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class SampleModel {
+
+    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE)
+    @Default(values="No resourceType")
+    protected String resourceType;
+
+    @OSGiService
+    private SlingSettingsService settings;
+    @SlingObject
+    private Resource currentResource;
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    @ValueMapValue
+    private String title;
+
+    @ValueMapValue
+    private String text;
+
+
+    private String message;
+
+    @PostConstruct
+    protected void init() {
+        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+        String currentPagePath = Optional.ofNullable(pageManager)
+                .map(pm -> pm.getContainingPage(currentResource))
+                .map(Page::getPath).orElse("");
+
+        message = "Hello World!\n"
+            + "Resource type is: " + resourceType + "\n"
+            + "Current page is:  " + currentPagePath + "\n"
+            + "This is instance: " + settings.getSlingId() + "\n";
+    }
+
+    public String getTitle(){
+        return StringUtils.isNotBlank(title) ? title : "defere value " ;
+
+    }
+
+    public String getText(){
+        return StringUtils.isNotBlank(text) ? text.toUpperCase() : null ;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
